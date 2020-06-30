3c0634610413e8a18a9b1dba673d8fc5a2609369
Douwe Maan <douwe@selenight.nl>
2020-06-30 10:54:54 -0500

    Only copy `.meltano/cache/discovery.yml` if it exists

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="files-docker",
-    version="0.1",
+    version="0.2",
     description="Meltano project files for Docker",
     packages=find_packages(),
     package_data={"bundle": ["Dockerfile", ".dockerignore"]},
